db3ff39b5ba9a742056dbfd4857c9fa9339e6bbf
Goofy <goofy.lp@gmail.com>
2017-05-24 17:34:28 +0800

    Update s3-dict.swift
    one pass solution

--- 1-two-sum/s3-dict.swift
+++ 1-two-sum/s3-dict.swift
@@ -1,17 +1,14 @@
-// https://leetcode.com/submissions/detail/103868770/
+// https://leetcode.com/submissions/detail/103869386/
 class Solution {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
         var dic = [Int:Int]()
-        for i in 0..<nums.count {
-            dic[nums[i]] = i
-        }
-        // let other = 0
         for idx in 0..<nums.count {
             if let otherIdx = dic[target-nums[idx]] {
                 if otherIdx != idx {
                     return [idx, otherIdx]
                 }
             }
+            dic[nums[idx]] = idx
         }
         return []
     }
