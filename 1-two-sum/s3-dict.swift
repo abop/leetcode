010444a526f5d602636115fec5c6aa8c200d5b62
Goofy <goofy.lp@gmail.com>
2017-05-24 17:22:30 +0800

    Update s3-dict.swift

--- 1-two-sum/s3-dict.swift
+++ 1-two-sum/s3-dict.swift
@@ -1,15 +1,13 @@
-// https://leetcode.com/submissions/detail/103868397/
+// https://leetcode.com/submissions/detail/103868770/
 class Solution {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
         var dic = [Int:Int]()
-        nums.enumerated().forEach({(idx, num) in 
-                dic[num] = idx
-        })
+        for i in 0..<nums.count {
+            dic[nums[i]] = i
+        }
         // let other = 0
         for idx in 0..<nums.count {
-            let num = nums[idx]
-            let otherNum = target-num
-            if let otherIdx = dic[otherNum] {
+            if let otherIdx = dic[target-nums[idx]] {
                 if otherIdx != idx {
                     return [idx, otherIdx]
                 }
